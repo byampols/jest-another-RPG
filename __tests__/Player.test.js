71330d06b0ba07d382c78d0589a80d3c14e0cb85
unknown <byampols@alumni.cmu.edu>
2021-10-04 17:09:14 -0400

    completed player methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,5 @@
 const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
 
@@ -30,4 +31,62 @@ test("gets inventory from player or returns false", () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
-})
+});
+
+test("adds a potion to inventory", () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test("uses a potion from the inventory", () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test("gets player's attack damage", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test("checks if player is alive or not", () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
